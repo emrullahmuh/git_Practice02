59c6e54753e1d33b54fa40a4f7278d511c559e08
emrulla muhter <emrullauigur@gmai.com>
2023-07-04 21:46:04 +0300

    login page is committed

--- src/Dat02/Logi_page.java
+++ src/Dat02/Logi_page.java
@@ -3,5 +3,7 @@ package Dat02;
 public class Logi_page {
     public static void main(String[] args) {
         System.out.println("for  login type your user name");
+   //slamura temoura shrimp is the
+
     }
 }
